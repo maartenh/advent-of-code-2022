96b3c96ff1a20be62bc5aee8bcbb7ec47df6ab03
Maarten Hazewinkel <maarten.hazewinkel@gmail.com>
2022-12-03 10:19:20 +0100

    Day 3 solutions

--- calendar/day03/Day3.kt
+++ calendar/day03/Day3.kt
@@ -5,10 +5,36 @@ import Lines
 
 class Day3 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        return input
+            .mapNotNull { line ->
+                val comp1 = line.substring(0, line.length / 2)
+                val comp2 = line.substring(line.length / 2, line.length)
+                val doubleItemType = comp1.first { comp2.contains(it) }
+                priority[doubleItemType]
+            }
+            .sum()
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input
+            .chunked(3)
+            .mapNotNull { triple ->
+                val commonItemType = triple[0].first {
+                    triple[1].contains(it) && triple[2].contains(it)
+                }
+                priority[commonItemType]
+            }
+            .sum()
+    }
+
+    private val priority = run {
+        val priorities = mutableMapOf<Char, Int>()
+        for ((index, type) in ('a'..'z').withIndex()) {
+            priorities.put(type, index + 1)
+        }
+        for ((index, type) in ('A'..'Z').withIndex()) {
+            priorities.put(type, index + 27)
+        }
+        priorities.toMap()
     }
 }

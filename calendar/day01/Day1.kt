4f93e7fcc30f5dca61bde19c14dfb2b57afc939d
Maarten Hazewinkel <maarten.hazewinkel@gmail.com>
2022-12-03 01:12:44 +0100

    Day 1 solutions

--- calendar/day01/Day1.kt
+++ calendar/day01/Day1.kt
@@ -2,13 +2,44 @@ package day01
 
 import Day
 import Lines
+import java.lang.Integer.min
 
 class Day1 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        val maxCarried = input
+            .splitAt { it.isEmpty() }
+            .map { list ->
+                list.sumOf { it.toInt() }
+            }
+            .max()
+
+        return maxCarried
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input
+            .splitAt { it.isEmpty() }
+            .map { list ->
+                list.sumOf { it.toInt() }
+            }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
+
+    fun <E> List<E>.splitAt(predicate: (E) -> Boolean): List<List<E>> {
+        val partitions = mutableListOf<List<E>>()
+
+        var source = this
+        while (source.isNotEmpty()) {
+            val part = source.takeWhile { !predicate(it) }
+            source = source.subList(min(part.size + 1, source.size), source.size)
+
+            if (part.isNotEmpty()) {
+                partitions.add(part)
+            }
+        }
+
+        return partitions.toList()
+   }
 }

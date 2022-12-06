3d6e6c283845e5f5213c2ff560e3c036cb058103
Maarten Hazewinkel <maarten.hazewinkel@gmail.com>
2022-12-06 19:06:53 +0100

    Day 4 solutions

--- calendar/day04/Day4.kt
+++ calendar/day04/Day4.kt
@@ -5,10 +5,27 @@ import Lines
 
 class Day4 : Day() {
     override fun part1(input: Lines): Any {
-        TODO("Not yet solved")
+        return input
+            .map(String::toSetPair)
+            .count {
+                it[0].containsAll(it[1]) || it[1].containsAll(it[0])
+            }
     }
 
     override fun part2(input: Lines): Any {
-        TODO("Not yet solved")
+        return input
+            .map(String::toSetPair)
+            .count {
+                it[0].intersect(it[1]).isNotEmpty()
+            }
     }
+
+}
+
+fun String.toSetPair(): List<Set<Int>> {
+    val parts = split(',', '-')
+    return listOf(
+        (parts[0].toInt() .. parts[1].toInt()).toSet(),
+        (parts[2].toInt() .. parts[3].toInt()).toSet()
+    )
 }
